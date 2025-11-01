59a78b3bbe7e50ad32917a5c7aca0572c8b8d357
Sammany1 <ialsammany@gmail.com>
2025-11-01 20:54:12 +0200

    Initialize project structure with .gitignore and empty module files

---
+++ compiler/__init__.py
@@ -0,0 +1,4 @@
+# This file is intentionally blank.
+# It tells Python that this 'compiler' directory should be
+# treated as a package, allowing you to use imports like:
+# from compiler.lexer import Lexer
