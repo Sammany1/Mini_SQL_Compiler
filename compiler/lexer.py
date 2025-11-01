ef602ac3f0931998f5672eda4b72f0aef0e62e73
samadessouky <s.mohamed2279@nu.edu.eg>
2025-11-02 00:19:32 +0200

    Edited lexer.py

---
+++ compiler/lexer.py
@@ -0,0 +1,127 @@
+from .tokens import Token, TokenType
+
+class Lexer:
+    """
+    The Lexical Analyzer. Reads source code and turns it into a stream
+    of tokens.
+    """
+    def __init__(self, source_code):
+        self.source = source_code
+        self.tokens = []
+        self.current_pos = 0
+        self.line = 1
+        self.column = 1
+        
+        # Keywords are case-sensitive [cite: 872, 875]
+        self.keywords = {
+            "SELECT": TokenType.SELECT,
+            "FROM": TokenType.FROM,
+            "WHERE": TokenType.WHERE,
+            "INSERT": TokenType.INSERT,
+            "INTO": TokenType.INTO,
+            "VALUES": TokenType.VALUES,
+            "UPDATE": TokenType.UPDATE,
+            "SET": TokenType.SET,
+            "DELETE": TokenType.DELETE,
+            "CREATE": TokenType.CREATE,
+            "TABLE": TokenType.TABLE,
+            "AND": TokenType.AND,
+            "OR": TokenType.OR,
+            "NOT": TokenType.NOT,
+            
+            # Map data types to the TYPE token [cite: 877]
+            "INT": TokenType.TYPE,
+            "FLOAT": TokenType.TYPE,
+            "TEXT": TokenType.TYPE,
+        }
+
+    def scan_tokens(self):
+        """Main loop: scans all tokens from the source code."""
+        while not self.is_at_end():
+            self.scan_token()
+        
+        # Add a final EOF token
+        self.tokens.append(Token(TokenType.EOF, "", self.line, self.column))
+        return self.tokens
+
+    def scan_token(self):
+        """Scans and adds the next single token."""
+        start_col = self.column
+        start_line = self.line
+        
+        char = self.advance()
+
+        # --- Single-character tokens ---
+        if char == '(': self.add_token(TokenType.LEFT_PAREN, '(')
+        elif char == ')': self.add_token(TokenType.RIGHT_PAREN, ')')
+        elif char == ',': self.add_token(TokenType.COMMA, ',')
+        elif char == ';': self.add_token(TokenType.SEMICOLON, ';')
+        elif char == '*': self.add_token(TokenType.STAR, '*')
+        elif char == '+': self.add_token(TokenType.PLUS, '+')
+        elif char == '/': self.add_token(TokenType.SLASH, '/')
+        
+        # --- One or two-character tokens ---
+        elif char == '=':
+            self.add_token(TokenType.EQUAL, '=')
+            
+        elif char == '!':
+            if self.peek() == '=':
+                self.advance() # consume the '='
+                self.add_token(TokenType.NOT_EQUAL, '!=')
+            else:
+                self.report_error(start_line, start_col, f"Invalid character '{char}'")
+
+        elif char == '<':
+            if self.peek() == '=':
+                self.advance() # consume the '='
+                self.add_token(TokenType.LESS_EQUAL, '<=')
+            elif self.peek() == '>':
+                self.advance() # consume the '>'
+                self.add_token(TokenType.NOT_EQUAL, '<>')
+            else:
+                self.add_token(TokenType.LESS_THAN, '<')
+                
+        elif char == '>':
+            if self.peek() == '=':
+                self.advance() # consume the '='
+                self.add_token(TokenType.GREATER_EQUAL, '>=')
+            else:
+                self.add_token(TokenType.GREATER_THAN, '>')
+
+        # --- Comments [cite: 889] ---
+        elif char == '-':
+            if self.peek() == '-':
+                self.handle_single_line_comment()
+            else:
+                self.add_token(TokenType.MINUS, '-')
+                
+        elif char == '#':
+            if self.peek() == '#':
+                self.handle_multiline_comment(start_line, start_col)
+            else:
+                self.report_error(start_line, start_col, f"Invalid character '{char}'")
+                self.add_token(TokenType.ILLEGAL, char)
+                
+        # --- Whitespace (ignored) [cite: 870] ---
+        elif char.isspace():
+            pass
+
+        # --- Multi-character tokens ---
+        elif char == "'":
+            self.handle_string(start_line, start_col)
+
+        elif char.isdigit():
+            self.current_pos -= 1 # Go back one to read the full number
+            self.column -= 1
+            self.handle_number()
+
+        elif char.isalpha():
+            self.current_pos -= 1 # Go back one to read the full identifier
+            self.column -= 1
+            self.handle_identifier()
+
+        # --- Errors ---
+        else:
+            # Catches illegal symbols like '@' or the '$' from the sample [cite: 893-894, 909]
+            self.report_error(start_line, start_col, f"Invalid character '{char}'")
+            self.add_token(TokenType.ILLEGAL, char)
